94f4f4362b742d4cf23e7f23c80b44966c711e0e
Marco Klein <hobart2967@outlook.com>
2026-06-07 01:15:25 +0200

    ci: Add publish workflow.

---
+++ .releaserc.js
@@ -0,0 +1,31 @@
+export default {
+	branches: ['main'],
+	plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'angular',
+                releaseRules: [
+                    { type: 'docs', scope: 'README', release: 'patch' },
+                    { type: 'refactor', release: 'patch' },
+                    { type: 'style', release: 'patch' },
+                ],
+                parserOpts: {
+                    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+                },
+            },
+        ],
+        '@semantic-release/release-notes-generator',
+        ['@semantic-release/github'],
+        ['@semantic-release/npm', {
+                'npmPublish': false
+            }],
+        ['@semantic-release/git', {
+        'assets': ['package.json'],
+        'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }],
+        ['@semantic-release/exec', {
+            'verifyReleaseCmd': 'echo ${nextRelease.version} > out/VERSION'
+        }]
+    ],
+};
